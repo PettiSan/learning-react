05146077fd4158c6be1d8602a07868a1a14a407d
Filipe Petitemberte <filipe.petitemberte@base.digitl>
2021-04-21 21:31:03 -0300

    exercise blog with redux-thunk & lodash

---
+++ blog/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostList from './PostList';
+
+const App = () => {
+  return (
+    <div className="ui container" style={{ marginTop: 25 }}>
+      <PostList />
+    </div>
+  );
+};
+
+export default App;
